55bcd68ebb2fbfc6d4f157a58edc6f5fb12dc771
Arjun Pandey <arjunpandey2205@gmail.com>
2025-06-27 13:34:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 2048 World - Homepage Script
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to 2048 World!");
+  
+  // You can add dynamic features later here
+});
